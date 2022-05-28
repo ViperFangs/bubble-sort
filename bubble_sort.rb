6f1349a523dba633bab852da955ce0484b06e810
Aarya <amathrej@asu.edu>
2022-05-28 02:27:52 -0700

    Use rubocop to fix format

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,18 @@
 def bubble_sort(unsorted_array)
-
   sorted_array = []
-  
-  unsorted_array.each_index {|index| sorted_array = bubble_sort_helper(unsorted_array)}
+  unsorted_array.each_index { sorted_array = bubble_sort_helper(unsorted_array) }
 
-  puts "#{sorted_array}"
-
+  puts sorted_array.to_s
 end
 
 def bubble_sort_helper(array)
-
-  array.each_index {|index| variable_switch(array, index) if swap_needed?(array, index)}
-
+  array.each_index { |index| variable_switch(array, index) if swap_needed?(array, index) }
 end
 
 def swap_needed?(array, index)
-
-  array[index + 1] == nil ? false : array[index] > array[index + 1]
-
+  array[index + 1].nil ? false : array[index] > array[index + 1]
 end
 
-def variable_switch(array , index)
-
+def variable_switch(array, index)
   array[index], array[index + 1] = array[index + 1], array[index]
-
 end
-
